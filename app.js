168c8d8a59ed317d4c2c15cdee26742368e109bc
Leandro-Mumbach <lean21mumbach@gmail.com>
2022-07-16 11:48:40 -0300

    add rama_lean

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
