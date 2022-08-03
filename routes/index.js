03d8560d4619c03ceec841cf2b20517f1b106e99
JoaquinAcosta <joaquinnof14@gmail.com>
2022-08-02 23:01:00 -0300

    add structure express-generator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const {index} = require('../controllers/indexController')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* / */
+router.get('/', index);
 
 module.exports = router;
