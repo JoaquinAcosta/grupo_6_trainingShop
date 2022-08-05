d03a77df8247154e70be8f8ae7ae76e7ecf9d100
JoaquinAcosta <joaquinnof14@gmail.com>
2022-08-04 23:35:30 -0300

    add controller and router productCart

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 /* var productsRouter = require('./controllers/productsController') */
+var productCartRouter = require('./routes/productCart');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/productCart', productCartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
