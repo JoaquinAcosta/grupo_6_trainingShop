788f3d52dd5dd6fa70f03f9e5d64456ace1923f2
fabiCoseglia <fabiancoseglia@gmail.com>
2022-08-19 16:04:05 -0300

    add todos los productos

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {detail, add, edit} = require ('../controllers/productsController')
+const {detail, add, edit, index} = require ('../controllers/productsController')
 
 router
     .get('/detail', detail)
     .get('/add', add)
     .get('/edit', edit)
+    //GET ALL PRODUCTS//
+    .get('/', index)
 
 
 module.exports = router;
