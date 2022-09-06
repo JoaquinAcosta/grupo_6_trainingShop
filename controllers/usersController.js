1ec14bf15db376e6a06fbb90c6717987b42248a8
fabiCoseglia <fabiancoseglia@gmail.com>
2022-09-06 01:00:35 -0300

    testing

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -81,7 +81,7 @@ module.exports = {
             const userlogged = users.find(user => user.id === req.session.userLogin.id)
             return res.render('profile', { errors: errors.mapped(), old: req.body, userlogged,users});
         }
-        
+        //test//
 		
     },
     processLogin: (req, res) => {
