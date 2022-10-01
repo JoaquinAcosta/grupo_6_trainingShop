62fcf31f8555d116de7a7efb92b5a97523b7ec53
JoaquinAcosta <joaquinnof14@gmail.com>
2022-09-30 23:35:56 -0300

    add src

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(methodOverride('_method'));
 app.use(session({ secret: 'this is secret', resave: false, saveUninitialized: true }));
 
