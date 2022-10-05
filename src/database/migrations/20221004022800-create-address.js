823b461a5b4708f7a5ff9f83ed04c7939f0b81d0
fabiCoseglia <fabiancoseglia@gmail.com>
2022-10-04 22:40:21 -0300

    add mayus to migrations

--- src/database/migrations/20221004022800-create-address.js
+++ src/database/migrations/20221004022800-create-address.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('addresses', {
+    await queryInterface.createTable('Addresses', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('addresses');
+    await queryInterface.dropTable('Addresses');
   }
 };
--- src/database/migrations/20221004023635-create-order.js
+++ src/database/migrations/20221004023635-create-order.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('orders', {
+    await queryInterface.createTable('Orders', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('orders');
+    await queryInterface.dropTable('Orders');
   }
 };
