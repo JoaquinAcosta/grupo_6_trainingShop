c1e7b9762fe8087d2c6d0065e71429dc93af117a
fabiCoseglia <fabiancoseglia@gmail.com>
2022-10-04 22:35:34 -0300

    correciones mayusculas

--- src/database/models/address.js
+++ src/database/models/address.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class address extends Model {
+  class Address extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  address.init({
+  Address.init({
     street: DataTypes.STRING,
     city: DataTypes.STRING,
     userId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'address',
+    modelName: 'Address',
   });
-  return address;
+  return Address;
 };
--- src/database/models/order.js
+++ src/database/models/order.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class order extends Model {
+  class Order extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  order.init({
+  Order.init({
     date: DataTypes.DATE,
     total: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'order',
+    modelName: 'Order',
   });
-  return order;
+  return Order;
 };
