7dd2da3a0d7d11b399e57d8961a1d152ff294198
JoaquinAcosta <joaquinnof14@gmail.com>
2022-10-10 23:29:47 -0300

    add edit

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,4 @@
-
+const db = require('../database/models')
 const { loadProducts, storeProducts } = require('../data/productsModule');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -13,10 +13,10 @@ module.exports = {
         })
     }, */
     detail: (req, res) => {
-		db.Product.findByPk(req.params.id,{
-			include : ['images']
-		})
-			.then(product => res. render('detail', {
+		db.Product.findByPk(req.params.id, {
+            include : ['images']
+        })
+			.then(product => res.render('detalledelproducto', {
 				product,
 				toThousand
 			}))
@@ -45,12 +45,37 @@ module.exports = {
         });
     },
     edit: (req, res) => {
-		const products= loadProducts()
+		/* const products= loadProducts()
 		const product = products.find(product => product.id === +req.params.id)
-		return res.render('editProduct',{product})
+		return res.render('editProduct',{product}) */
+        let categories = db.Category.findAll({
+            attributes : ['id','name'],
+            order : ['name']
+        });
+        let sections = db.Section.findAll({
+            attributes : ['id','name'],
+            order : ['name']
+        });
+        let brands = db.Brand.findAll({
+            attributes : ['id','name'],
+            order : ['name']
+        });
+        
+        let product = db.Product.findByPk(req.params.id);
+
+        Promise.all([categories,product,sections,brands])
+            .then(([categories,product,sections,brands]) => {
+                return res.render('editProduct', {
+                    product,
+                    categories,
+                    sections,
+                    brands
+                })
+            })
+            .catch (error => console.log(error))
 	},
 	update: (req, res) => {
-		const products= loadProducts()
+		/* const products= loadProducts()
 		const {name, price, category, description, brand, section} = req.body
 		const productsModify = products.map(product => {
 			if(product.id === +req.params.id){
@@ -66,7 +91,22 @@ module.exports = {
 			return product
 		})
 		storeProducts(productsModify)
-		return res.redirect('/admin')
+		return res.redirect('/admin') */
+        db.Product.update(
+        {
+            ...req.body,
+            name : req.body.name.trim(),
+            description : req.body.description.trim()
+
+        },
+        {
+            where : {
+                id : req.params.id
+            }
+        }
+    )
+        .then( () => res.redirect('/admin'))
+        .catch((error) => console.log(error))
 	},
     store: (req, res) => {
         
--- src/data/productsModule.js
+++ src/data/productsModule.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const loadProducts = () => {
-    const productsFilePath = path.join(__dirname, 'productDataBase.json');
+    const productsFilePath = path.join(__dirname, 'products.json');
     const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
     return products
 }
 
 const storeProducts = (products) => {
-    fs.writeFileSync(path.join(__dirname,'productDataBase.json'), JSON.stringify(products, null, 3), 'utf-8');
+    fs.writeFileSync(path.join(__dirname,'products.json'), JSON.stringify(products, null, 3), 'utf-8');
 }
 
 module.exports = {
