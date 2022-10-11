2b4439dc3a0d8da2266c9fb0e1c2a107f7bdd05b
fabiCoseglia <fabiancoseglia@gmail.com>
2022-10-10 23:13:13 -0300

    add multer to product and CREATE of crud

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,10 @@
-
+const db = require('../database/models');
+const { decodeBase64 } = require('bcryptjs');
+const { promiseImpl } = require('ejs');
 const { loadProducts, storeProducts } = require('../data/productsModule');
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+// require db.models
+
 
 
 module.exports = {
@@ -14,7 +18,34 @@ module.exports = {
 
     },
     add: (req, res) => {
-        return res.render ('addProduct')
+        let categories= db.Category.findAll({
+            attributes: ['id','name'],
+            order: ['name']
+        })
+        
+        let sections = db.Section.findAll({
+            attributes: ['id', 'name'],
+            order: ['name']
+        }) 
+
+        let brands = db.Brand.findAll({
+            attributes: ['id','name'],
+            order: ['name']
+        })
+
+        Promise.all([categories,sections,brands])
+			.then(([categories,sections,brands]) => {
+				return res.render('addProduct',{
+					categories,
+                    sections,
+                    brands
+				})
+			})
+			.catch(error => console.log(error));
+
+
+        
+        
     },
     destroy : (req,res) => {
 
@@ -60,26 +91,30 @@ module.exports = {
 		return res.redirect('/admin')
 	},
     store: (req, res) => {
-        
-        const {id,name,description,image,category,price,brand, section} = req.body
-        const products = loadProducts();
 
-        const newProduct = {
-            id : (products[products.length - 1].id + 1),
-            name : name.trim(),
-            description : description.trim(),
-            image : 'default-image.png',
-            category,
-            price : +price,
-            brand,
-            section
-        }
+        db.Product.create({
+			...req.body,
+			name : req.body.name,
+			description : req.body.description,
 
-        const productsModify = [...products, newProduct];
-
-        storeProducts(productsModify);
-
-        return res.redirect('/admin')
+		})
+        .then(product => {
+            
+            if(req.files.length){
+                let images = req.files.map(({filename}) => {
+                    return {
+                        file : filename,
+                        productId: product.id
+                    }
+                })
+                db.Image.bulkCreate(images,{
+                    validate : true
+                }).then( (result) => console.log(result) )
+                return res.redirect('/products')
+            }
+            
+        })
+        .catch(error => console.log(error))
 
     },
  } 
