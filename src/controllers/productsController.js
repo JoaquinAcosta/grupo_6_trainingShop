2c0e92b7c1b9426ef52962ed5482de30efa45de0
DanielZ12 <zambranadanieljh@gmail.com>
2022-10-15 00:43:39 -0300

    new Brand terminado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,138 +1,139 @@
-const db = require('../database/models');
-const { decodeBase64 } = require('bcryptjs');
-const { promiseImpl } = require('ejs');
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
+const db = require("../database/models");
+const { decodeBase64 } = require("bcryptjs");
+const { promiseImpl } = require("ejs");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
- 
-    detail: (req, res) => {
-		db.Product.findByPk(req.params.id, {
-            include : ['images']
+  detail: (req, res) => {
+    db.Product.findByPk(req.params.id, {
+      include: ["images"],
+    })
+      .then((product) =>
+        res.render("detalledelproducto", {
+          product,
+          toThousand,
         })
-			.then(product => res.render('detalledelproducto', {
-				product,
-				toThousand
-			}))
-			.catch(error => console.log(error))
-	},
-    add: (req, res) => {
-        let categories= db.Category.findAll({
-            attributes: ['id','name'],
-            order: ['name']
-        })
-        
-        let sections = db.Section.findAll({
-            attributes: ['id', 'name'],
-            order: ['name']
-        }) 
+      )
+      .catch((error) => console.log(error));
+  },
+  add: (req, res) => {
+    let categories = db.Category.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
 
-        let brands = db.Brand.findAll({
-            attributes: ['id','name'],
-            order: ['name']
-        })
+    let sections = db.Section.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
 
-        Promise.all([categories,sections,brands])
-			.then(([categories,sections,brands]) => {
-				return res.render('addProduct',{
-					categories,
-                    sections,
-                    brands
-				})
-			})
-			.catch(error => console.log(error));        
-    },
-    
-    index: (req,res) => {
-        db.Product.findAll({
-            include : ['images']
-        })
-            .then(products => {
-                res.render('products', { products, toThousand })
-            })
-            .catch(error => console.log(error))
-        
-    },
-    edit: (req, res) => {
-        let categories = db.Category.findAll({
-            attributes : ['id','name'],
-            order : ['name']
+    let brands = db.Brand.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
+
+    Promise.all([categories, sections, brands])
+      .then(([categories, sections, brands]) => {
+        return res.render("addProduct", {
+          categories,
+          sections,
+          brands,
         });
-        let sections = db.Section.findAll({
-            attributes : ['id','name'],
-            order : ['name']
-        });
-        let brands = db.Brand.findAll({
-            attributes : ['id','name'],
-            order : ['name']
-        });
-        
-        let product = db.Product.findByPk(req.params.id);
+      })
+      .catch((error) => console.log(error));
+  },
 
-        Promise.all([categories,product,sections,brands])
-            .then(([categories,product,sections,brands]) => {
-                return res.render('editProduct', {
-                    product,
-                    categories,
-                    sections,
-                    brands
-                })
-            })
-            .catch (error => console.log(error))
-	},
-	update: (req, res) => {
-        db.Product.update(
-        {
-            ...req.body,
-            name : req.body.name.trim(),
-            description : req.body.description.trim() 
+  index: (req, res) => {
+    db.Product.findAll({
+      include: ["images"],
+    })
+      .then((products) => {
+        res.render("products", { products, toThousand });
+      })
+      .catch((error) => console.log(error));
+  },
+  edit: (req, res) => {
+    let categories = db.Category.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
+    let sections = db.Section.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
+    let brands = db.Brand.findAll({
+      attributes: ["id", "name"],
+      order: ["name"],
+    });
 
+    let product = db.Product.findByPk(req.params.id);
+
+    Promise.all([categories, product, sections, brands])
+      .then(([categories, product, sections, brands]) => {
+        return res.render("editProduct", {
+          product,
+          categories,
+          sections,
+          brands,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  update: (req, res) => {
+    db.Product.update(
+      {
+        ...req.body,
+        name: req.body.name.trim(),
+        description: req.body.description.trim(),
+      },
+      {
+        where: {
+          id: req.params.id,
         },
-        {
-            where : {
-                id : req.params.id
-            }
-        }
+      },
+      
     )
-        .then( () => res.redirect('/admin'))
-        .catch((error) => console.log(error))
-	},
-    store: (req, res) => {
+      .then(() => res.redirect("/admin"))
+      .catch((error) => console.log(error));
+  },
+  store: async (req, res) => {
+    let { brandId, otro } = req.body
+    let new_brand;
+    try {
+        if (brandId == "" && otro) {
+            new_brand = await db.Brand.create({name: otro})
+            new_brand = new_brand.id
+        }
 
-        db.Product.create({
-			...req.body,
-			name : req.body.name,
-			description : req.body.description,
-
-		})
-        .then(product => {
-            
-            if(req.files.length){
-                let images = req.files.map(({filename}) => {
-                    return {
-                        file : filename,
-                        productId: product.id
-                    }
-                })
-                db.Image.bulkCreate(images,{
-                    validate : true
-                }).then( (result) => console.log(result) )
-                return res.redirect('/admin')
-            }
-            
+        let new_product = await db.Product.create({
+             ...req.body,
+             name : req.body.name,
+            brandId: new_brand ? new_brand : brandId,
+             description : req.body.description,
         })
-        .catch(error => console.log(error))
+        let images = req.files.map(({filename}) => {
+            return {
+                file: filename,
+                productId: new_product.id
+            }
+        });
+        let result = await db.Image.bulkCreate(images, { validate: true});
+        console.log(result);
+        return res.redirect('/admin')
+    }
 
-    },
+    catch (error) {
+        console.log(error);
+    }
+},
 
-    destroy: function (req, res) {
-        db.Product.destroy({
-			where : {
-				id : req.params.id
-			}
-		})
-			.then( () => res.redirect('/admin'))
-			.catch( error => console.log(error));
-	}
-  
+  destroy: function (req, res) {
+    db.Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then(() => res.redirect("/admin"))
+      .catch((error) => console.log(error));
+  },
 };
