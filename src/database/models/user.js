82a72943fa9769390a454cb76952f48b7b1f1e69
FloraSoledad <vargas.flora@hotmail.com>
2022-11-18 15:51:36 -0300

    validaciones login de usuarios

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, UnknownConstraintError
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -9,6 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+    existEmail(value) {
+      return new Promise ((resolve, reject) => {
+        const user = User.findOne ({where:{email:value}})
+        if(user){
+          resolve(true)
+        }
+        reject(false)
+      })
+    }
+
+
+
     static associate(models) {
       // define association here
       User.hasMany(models.Address,{
@@ -27,23 +40,67 @@ module.exports = (sequelize, DataTypes) => {
   }
   /* login de usuario */
   User.init({
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
     validate: {
       is: {
         args: /^[a-z]+$/i,
         msg: "No debe contener numeros(name)"
       }
+    }
     },
-    lastName: DataTypes.STRING,
-    validate: {
-      is: {
-        args: /^[a-z]+$/i,
-        msg: "No debe contener numeros (lastName)"
+    lastName: {
+      type: DataTypes.STRING,
+      validate: {
+        is: {
+          args: /^[a-z]+$/i,
+          msg: "No debe contener numeros(lastName)"
+        }
       }
     },
+   
     avatar: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+
+
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Ingrese un email valido",
+          async email(value){ "emailemail"  /* : Unknown word. */
+          const exist = await this.existEmail(value)
+          if(exist){
+            throw new error('El mail ya existe');
+          }
+          }
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isNull: {
+          args: true,
+          msg: "No puede ser nulo"
+        },
+        notEmpty: {
+          args: true,
+          msg: "No puede ser nulo"
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: "Contraseña invalida"
+        }
+      }
+    },
+    
+
+
     phone: DataTypes.INTEGER,
     rolId: DataTypes.INTEGER,
  
