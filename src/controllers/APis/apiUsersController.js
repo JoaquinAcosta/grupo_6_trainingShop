89bb35b415548864b527f62e512d5458a7ed0337
Leandro-Mumbach <lean21mumbach@gmail.com>
2022-11-19 00:23:46 -0300

    add new config api list user

--- src/controllers/APis/apiUsersController.js
+++ src/controllers/APis/apiUsersController.js
@@ -1,11 +1,24 @@
 const db = require("../../database/models");
 
+const { unlinkSync } = require("fs");
+const path = require("path");
+const { literal, Op } = require("sequelize");
+const { literalQueryUrlImage } = require("../../helpers/literalQueryUrlImage");
+const { sendJsonError } = require("../../helpers/sendJsonError");
+
 module.exports = {
+    image: (req, res) => {
+        res.sendFile(
+          path.join(__dirname, `../../../public/images/productsImage/${req.params.img}`)
+        );
+      },
+
     getById : async (req, res) => {
         try{
             const user = await db.User.findByPk(req.params.id,{ 
                 attributes: {
                     association: "images",
+                    include: [ literalQueryUrlImage(req, "avatar", "avatar", "/users")],
                     exclude: ["createdAt", "updatedAt", "deletedAt", "productId", "password", "rolId"]
                             }
                 })
