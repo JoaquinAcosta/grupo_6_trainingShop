0067b5550591f774003a0bfa8a882c6324905b0f
FloraSoledad <vargas.flora@hotmail.com>
2022-11-22 22:19:41 -0300

    login de usuario

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,15 @@
 'use strict';
-const {
-  Model, UnknownConstraintError
-} = require('sequelize');
+const { hashSync } = require('bcryptjs');
+const { Model, /* UnknownConstraintError */} = require('sequelize');
+const { objectValidate, defaultValidationsRequiredFields } = require('../resource');
+const {unlinkSync} =require ('fs')
+const {join} = require ('path')
+
+
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
+    /*
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
@@ -13,10 +18,9 @@ module.exports = (sequelize, DataTypes) => {
     existEmail(value) {
       return new Promise ((resolve, reject) => {
         const user = User.findOne ({where:{email:value}})
-        if(user){
-          resolve(true)
-        }
-        reject(false)
+
+          resolve(user)
+    
       })
     }
 
@@ -43,61 +47,63 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
     validate: {
-      is: {
-        args: /^[a-z]+$/i,
-        msg: "No debe contener numeros(name)"
+      is: objectValidate(/^[a-z]+$/i,"No debe contener numeros(name)")
       }
-    }
     },
     lastName: {
       type: DataTypes.STRING,
       validate: {
-        is: {
-          args: /^[a-z]+$/i,
-          msg: "No debe contener numeros(lastName)"
-        }
+        is: objectValidate(/^[a-z]+$/i,"No debe contener numeros(lastName)")
       }
     },
    
-    avatar: DataTypes.STRING,
-
-
-
+    avatar: 
+    {
+      type: DataTypes.STRING,
+      validate: {
+        isImage(value){
+          if(!/.png|.jpg|.jpeg|.webp/i.test(value)){
+            unlinkSync(join)
+            throw new Error ("Archivo invalido")
+          }
+        }
+      }
+    },
+      
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: {
-          args: true,
-          msg: "Ingrese un email valido",
-        },
-        async email(value){ /* "emailemail" : Unknown word.  */
-        const exist = await this.existEmail(value)
-        if(exist){
-          throw new error('El mail ya existe');
-        }
-        }
-      }
-    
+          ...defaultValidationsRequiredFields,
+          isNull:objectValidate(false,"Ingrese un mail valido"),
+
+          async email(value){
+            const exist = await this.existEmail(value)
+            if(exist){
+              throw new Error('El mail ya existe')
+            }
+          }
+      }  
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isNull: {
-          args: true,
-          msg: "No puede ser nulo"
-        },
-        notEmpty: {
-          args: true,
-          msg: "No puede ser nulo"
-        },
-        isAlphanumeric: {
-          args: true,
-          msg: "Contraseña invalida"
-        }
-      }
+        ...defaultValidationsRequiredFields,
+
+       isAlphanumeric: objectValidate( true, "Contraseña invalida, solo numeros y letras"),
+       len: objectValidate ([8,16],"longitud invalida, (mas de 8 y menos de 16) "),
+       is: objectValidate(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/ , "la contrasenia debe tener entre 8 y 16 caracteres, almenos una minuscula,almenos una mayuscula"),
+
+       hashPass(value){
+        User.beforeCreate((user)=> {
+          user.password =hashSync(value)
+        })
+       }
+
+
+      },
     },
     
 
