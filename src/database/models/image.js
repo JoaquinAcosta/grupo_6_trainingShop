aac9d3615a79e36a5d2c8677d6ebe0073af3974c
fabiCoseglia <fabiancoseglia@gmail.com>
2022-11-23 00:33:47 -0300

    fix error apis

--- src/database/models/image.js
+++ src/database/models/image.js
@@ -13,16 +13,29 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Image.belongsTo(models.Product,{
         as: 'products',
-        foreignKey: 'productId'
+        foreignKey: 'productId',
+        onDelete: 'cascade'
       });
     }
   }
   Image.init({
-    file: DataTypes.STRING,
+    file: {
+      type: DataTypes.STRING,
+      defaultValue: "default-image.png",
+      validate: {
+        isImage(value){
+          if(!/.png|.jpg|.jpeg|.webp/i.test(value)){
+            unlinkSync(join(__dirname,`../../../public/images/productsImage/${value}`)) 
+            throw new Error("Archivo invalido")
+          }
+        }
+      }
+     },
     productId: DataTypes.INTEGER
-  }, {
+  },{
     sequelize,
     modelName: 'Image',
+    paranoid: true
     
   });
   return Image;
