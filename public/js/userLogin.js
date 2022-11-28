f09e22e782aeb47e3b8b5586714344baf2ff9a7a
FloraSoledad <vargas.flora@hotmail.com>
2022-11-28 00:05:10 -0300

    validaciones login

--- public/js/userLogin.js
+++ public/js/userLogin.js
@@ -11,29 +11,6 @@ window.onload= function(){
     let passwordError= document.getElementById("passwordError")
 
 
-//llamado a la Api
-const verifyEmail = async (email) => {
-    try{
-        const data = JSON.stringify
-        
-        let response = await fetch ('/api/users/check-email', {
-            method : 'POST',
-            body : JSON.stringify({
-                email : email
-            }),
-            headers : {
-                'content-Type' : 'aplication/json'
-            }
-        });
-            let result = await response.json();
-            return result.data;
-
-        } catch (error){
-            console.error
-    }
-}
-
-
         //email validations
 email.addEventListener("focus", function () {
 emailError.innerHTML = "";
@@ -42,7 +19,7 @@ email.classList.remove("is-valid") || email.classList.remove("is-invalid");
 
 
 
-email.addEventListener("blur", async function (e) {
+email.addEventListener("blur",  function (e) {
 switch (true) {
 case exRegEmail.test(email.value):
     email.classList.add("is-valid");
@@ -55,15 +32,7 @@ case !exRegEmail.test(this.value):
     email.classList.add("is-invalid");
     emailError.innerHTML = "El email tiene un formato invalido";
     break;
-case await verifyEmail(this.value):
-    email.classList.add("is-invalid");
-    emailError.innerHTML = "El email ya se encuentra registrado";
-    break;
-default:
-    varidFied('errorEmail')
-    break;
-}
-/* console.log(email.value) */
+} 
 });
 
 
@@ -89,7 +58,7 @@ formulario.addEventListener("submit", function (e) {
 
 let inputsValid = document.querySelectorAll(".is-valid");
 let inputsInvalid = document.querySelectorAll('.is-invalid');
-if (inputsValid.length < 1 || inputsInvalid.length > 0) {
+if ( password.value ===""|| inputsValid.length < 1 || inputsInvalid.length > 0) {
 console.log("disable");
 e.preventDefault();
 }
