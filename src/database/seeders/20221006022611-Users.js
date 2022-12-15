085b6ffcd3131d653a66a33d41d1953b01d39517
JoaquinAcosta <joaquinnof14@gmail.com>
2022-12-14 22:15:32 -0300

    corretion seeders user

--- src/database/seeders/20221006022611-Users.js
+++ src/database/seeders/20221006022611-Users.js
@@ -28,7 +28,7 @@ const users = [
     email : 'joaquin@gmail.com',
     password : hashSync("123Aa!",10),
     avatar : null,
-    phone : Acosta,
+    phone : 345678,
     rolId : 1,
     createdAt : new Date()
   },
