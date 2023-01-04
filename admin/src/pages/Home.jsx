cb5506fe04cc468dc607a7812b17fafaffe12e56
fabiCoseglia <fabiancoseglia@gmail.com>
2023-01-04 01:06:55 -0300

    comsume api to react front-end for last product

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Categories } from '../components/categories/Categories';
+import { LastProduct } from '../components/lastProduct/LastProduct';
 import {Metrics} from '../components/metricas/Metrics'
 
 export const Home = () => {
@@ -16,20 +17,7 @@ export const Home = () => {
 
     <div className="row">
 
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Último Producto Agregado</h5>
-                </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: "40rem" }} src="assets/images/mandalorian.jpg" alt=" Star Wars - Mandalorian " />
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-                </div>
-            </div>
-        </div>
+       <LastProduct/>
 
     <Categories/>
 
