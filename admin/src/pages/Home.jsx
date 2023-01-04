90337cb36297748f8a28a5d39a29f1ba8674d123
JoaquinAcosta <joaquinnof14@gmail.com>
2023-01-03 22:41:54 -0300

    add endpoint newest product

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -19,7 +19,7 @@ export const Home = () => {
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
+                    <h5 className="m-0 font-weight-bold text-gray-800">Último Producto Agregado</h5>
                 </div>
                 <div className="card-body">
                     <div className="text-center">
